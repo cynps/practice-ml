7a879535f066c8356c9611a248fa70f3d322b523
cynps <cnospy@gmail.com>
2023-01-19 18:43:57 +0900

    upd speechrecognition

--- speechrecognition/voice_test.py
+++ speechrecognition/voice_test.py
@@ -1,17 +1,31 @@
 from os import path
 import speech_recognition as sr
 
+# variant 
+rangeparam = 13
+
 r = sr.Recognizer()
 
-audio_file = path.join(path.dirname(path.realpath(__file__)), "sample.wav")
-print(audio_file)
-
-with sr.AudioFile(audio_file) as source:
-  audio = r.record(source)
-
 try:
-  text = r.recognize_google(audio, language='ja-JP')
-  print(text)
+  for i in range(rangeparam):
+    f = open("dist/output/result.txt", "a")
+
+    audio_file = path.join(path.dirname(path.realpath(__file__)), "dist/output/output_{}.wav".format(i))
+    print(audio_file)
+
+    with sr.AudioFile(audio_file) as source:
+      audio = r.record(source)
+
+    text = r.recognize_google(audio, language='ja-JP')
+    f.write(text)
+    f.write("\r\n")
+    
+    f.close()
+
+  f = open("dist/output/result.txt", "a")
+  f.write("\r\n")
+  f.close()
+
 except sr.UnknownValueError:
   print("error")
 except sr.RequestError as e:

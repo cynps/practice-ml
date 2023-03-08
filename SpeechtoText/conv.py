c31477fe306d9f0e0e27dea1c55467dd7cc2e413
cynps <cnospy@gmail.com>
2023-03-08 20:41:11 +0900

    upd: 動画ファイルコンバーターのアップデート
    複数ファイル対応
    exe ファイル追加（conv.zip）

--- SpeechtoText/conv.py
+++ SpeechtoText/conv.py
@@ -8,31 +8,27 @@ import math
 config = configparser.ConfigParser()
 config.read('config.ini', 'UTF-8')
 path = config['DEFAULT']['source_file_path']
-filename = config['VARIANT']['source_file_name']
-filename_len = len(filename)
+except_filename = config['DEFAULT']['except_file_name']
+except_filename_len = len(except_filename)
 
 files = glob.glob(path + "*")
 srcfilepath = ""
 for file in files:
-  if file[len(file)-filename_len:] == filename:
+  target_filename = file[len(file)-except_filename_len:]
+  if file[len(file)-except_filename_len:] != except_filename:
     print(file)
-    srcfilepath = path + filename
+    srcfilepath = file
 
-video = cv2.VideoCapture(srcfilepath)
-frame_rate = video.get(cv2.CAP_PROP_FPS)
-frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
-sec = frame_count / frame_rate
-stride = int(config['VARIANT']['stride'])
-rangeparam = math.ceil(sec / stride)
+    video = cv2.VideoCapture(srcfilepath)
+    frame_rate = video.get(cv2.CAP_PROP_FPS)
+    frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
+    sec = frame_count / frame_rate
+    stride = int(config['VARIANT']['stride'])
+    rangeparam = math.ceil(sec / stride)
 
-print("srcfilepath", srcfilepath)
-print("partitions:", rangeparam)
-
-# variant
-for i in range(rangeparam):
-  start = int(i * stride)
-  stream = ffmpeg.input(srcfilepath, ss=start, t=stride)
-  stream = ffmpeg.output(stream, "dist/output/output_{}.wav".format(i), format="wav")
-  ffmpeg.run(stream)
-
-
+    # variant
+    for i in range(rangeparam):
+      start = int(i * stride)
+      stream = ffmpeg.input(srcfilepath, ss=start, t=stride)
+      stream = ffmpeg.output(stream, "dist/output/{}_output_{}.wav".format(target_filename, i), format="wav")
+      ffmpeg.run(stream)
